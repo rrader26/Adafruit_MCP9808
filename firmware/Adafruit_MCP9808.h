0c47b07c0b9ebd06f316e40858ec8605898ae4a7
Ryan Rader <rkrader@clt-mac-C02P6APGG3QD.attlocal.net>
2016-11-04 16:53:53 -0400

    fix

--- firmware/Adafruit_MCP9808.h
+++ firmware/Adafruit_MCP9808.h
@@ -17,18 +17,9 @@
 */
 /**************************************************************************/
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
 
-#ifdef __AVR_ATtiny85__
- #include "TinyWireM.h"
- #define Wire TinyWireM
-#else
- #include <Wire.h>
-#endif
+ #include "Particle.h"
+
 
 #define MCP9808_I2CADDR_DEFAULT        0x18
 #define MCP9808_REG_CONFIG             0x01
